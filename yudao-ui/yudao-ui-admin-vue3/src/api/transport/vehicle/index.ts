237fbe8df1ddeaa0e8189282a78f4ff44842e0b2
ycg-coder <2927449715@qq.com>
2026-08-04 23:55:51 +0800

    fix: missing vehicle API file

---
+++ yudao-ui/yudao-ui-admin-vue3/src/api/transport/vehicle/index.ts
@@ -0,0 +1,27 @@
+import request from '@/config/axios'
+import type { PageParam } from '@/types/common'
+
+export interface VehicleVO {
+  id?: number
+  plateNo: string
+  vehicleType?: number
+  passengerCapacity?: number
+  cargoCapacityKg?: number
+  createTime?: string
+}
+
+export const getVehiclePage = (params: PageParam & Partial<VehicleVO>) => {
+  return request.get({ url: '/transport/vehicle/page', params })
+}
+export const getVehicle = (id: number): Promise<VehicleVO> => {
+  return request.get({ url: '/transport/vehicle/get', params: { id } })
+}
+export const createVehicle = (data: VehicleVO) => {
+  return request.post({ url: '/transport/vehicle/create', data })
+}
+export const updateVehicle = (data: VehicleVO) => {
+  return request.put({ url: '/transport/vehicle/update', data })
+}
+export const deleteVehicle = (id: number) => {
+  return request.delete({ url: '/transport/vehicle/delete', params: { id } })
+}
