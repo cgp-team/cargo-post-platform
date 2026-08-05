875b59565d9d9b94675958eecbfbac631be0203e
ycg-coder <2927449715@qq.com>
2026-08-05 21:10:31 +0800

    fix: add createTime field to PageReqVO and fix TS undefined types in forms

--- yudao-module-transport/src/main/java/cn/iocoder/yudao/module/transport/controller/admin/transport/order/vo/TransportOrderPageReqVO.java
+++ yudao-module-transport/src/main/java/cn/iocoder/yudao/module/transport/controller/admin/transport/order/vo/TransportOrderPageReqVO.java
@@ -5,6 +5,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.time.LocalDateTime;
+
+import static cn.iocoder.yudao.framework.common.util.date.DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND;
 
 @Schema(description = "管理后台 - 订单分页 Request VO")
 @Data
@@ -21,4 +25,8 @@ public class TransportOrderPageReqVO extends PageParam {
     private Long pickupStationId;
     @Schema(description = "送达站点编号")
     private Long deliveryStationId;
+
+    @DateTimeFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
+    @Schema(description = "创建时间")
+    private LocalDateTime[] createTime;
 }
--- yudao-module-transport/src/main/java/cn/iocoder/yudao/module/transport/controller/admin/transport/shift/vo/ShiftPageReqVO.java
+++ yudao-module-transport/src/main/java/cn/iocoder/yudao/module/transport/controller/admin/transport/shift/vo/ShiftPageReqVO.java
@@ -5,6 +5,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.time.LocalDateTime;
+
+import static cn.iocoder.yudao.framework.common.util.date.DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND;
 
 @Schema(description = "管理后台 - 班次分页 Request VO")
 @Data
@@ -17,4 +21,8 @@ public class ShiftPageReqVO extends PageParam {
     private Long routeId;
     @Schema(description = "班次状态")
     private Integer status;
+
+    @DateTimeFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
+    @Schema(description = "创建时间")
+    private LocalDateTime[] createTime;
 }
